8666985b91b6d7b381665d8cba1ff5a7ffbf4e7a
Kevin Burleigh <klb@kindling>
2019-05-08 00:11:50 -0500

    fun with transactions

--- rails/myproject/spec/exper/transaction_spec.rb
+++ rails/myproject/spec/exper/transaction_spec.rb
@@ -16,3 +16,39 @@ RSpec.describe 'transactions' do
     end
   end
 end
+
+RSpec.describe 'transaction oddities' do
+  context 'rolling back a nested transaction' do
+    it 'probably does not do what you were expecting' do
+      JunkRecord.transaction do
+        JunkRecord.create!(name: 'foo')
+        JunkRecord.transaction do
+          JunkRecord.create!(name: 'bar')
+          raise ActiveRecord::Rollback
+        end
+      end
+
+      expect(JunkRecord.count).to eq(2)
+    end
+  end
+
+  context 'rolling back a nested transaction with requires_new set' do
+    it 'behaves as expected' do
+      JunkRecord.transaction do
+        JunkRecord.create!(name: 'foo')
+        JunkRecord.transaction(requires_new: true) do
+          JunkRecord.create!(name: 'bar')
+          raise ActiveRecord::Rollback
+        end
+      end
+
+      expect(JunkRecord.count).to eq(1)
+    end
+  end
+end
+
+RSpec.describe 'example transaction detection' do
+  it 'is inside a transaction' do
+    expect(ActiveRecord::Base.connection.open_transactions).to_not eq(0)
+  end
+end
