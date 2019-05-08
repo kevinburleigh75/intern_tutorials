9c7420f196967ec650dc1d26048f0d0db0e85cc5
Kevin Burleigh <klb@kindling>
2019-05-08 00:50:32 -0500

    some refactoring and new failing spec

--- rails/myproject/spec/api/create_api_spec.rb
+++ rails/myproject/spec/api/create_api_spec.rb
@@ -2,28 +2,20 @@ require 'rails_helper'
 
 RSpec.describe '/create API endpoint', type: :request do
   context 'when sent valid input' do
+    let(:payload) {
+      { names: ['Alice','Bob','Charlie','Debbie'] }
+    }
+
     it 'returns status 200 (success)' do
-      payload = {
-        names: [
-          'Alice',
-          'Bob',
-          'Charlie',
-          'Debbie',
-        ]
-      }
       response_status, response_body = create_request(request_payload: payload)
       expect(response_status).to eq(200)
     end
 
+    it 'creates the appropriate number of records' do
+      expect{create_request(request_payload: payload)}.to change{JunkRecord.count}.by(4)
+    end
+
     it 'returns the number of created records' do
-      payload = {
-        names: [
-          'Alice',
-          'Bob',
-          'Charlie',
-          'Debbie',
-        ]
-      }
       response_status, response_body = create_request(request_payload: payload)
       expect(response_body[:num_created]).to eq(4)
     end
