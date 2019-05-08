e549eb56c58718c01be9ce92e56eb2218a53f195
Kevin Burleigh <klb@kindling>
2019-05-07 22:37:03 -0500

    create JunkRecord model and run migrations

--- rails/myproject/db/migrate/20190508032752_create_junk_records.rb
+++ rails/myproject/db/migrate/20190508032752_create_junk_records.rb
@@ -1,6 +1,8 @@
 class CreateJunkRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :junk_records do |t|
+      t.string :name, null: false
+      t.timestamps
     end
   end
 end
