9b8c14367fb3c6e8929ba65dd29b0e8d6a261c71
Kevin Burleigh <klb@kindling>
2019-05-10 23:44:43 -0500

    add more CreateService specs

--- rails/myproject/spec/services/create_service_spec.rb
+++ rails/myproject/spec/services/create_service_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe CreateService do
       expect(CreateService.new.process({names: []})).to eq({num_created: 0})
     end
   end
+
+  context 'when previously-unseen names are given' do
+    it 'creates one JunkRecord for each given name' do
+      expect{CreateService.new.process({names: ['Alice', 'Bob']})}.to change{JunkRecord.count}.by(2)
+    end
+    it 'sets num_created equal to the number of created JunkRecords' do
+      expect(CreateService.new.process({names: ['Alice', 'Bob']})).to eq({num_created: 2})
+    end
+  end
 end
